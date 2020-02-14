d668c2c9eeeeece868e44f88cb04b6de531a5d7e
Ryan Brooks <ryan.brooks@outlook.com>
2020-02-13 19:05:36 -0600

    Updating with Exercise 1.

---
+++ Learn-Python/exercise1.py
@@ -0,0 +1,20 @@
+#Basic demonstration of Hello World.
+print("'Hello, World!'")
+
+
+#
+#   Ryans Banana Farm
+#
+
+
+#57 trees with eight bananas on each tree. 
+bananas = 8 * 57
+
+#Monkeys love bananas as we all know.  We have four monkeys living in our banana farm.
+monkeys = 4
+
+#Let's figure out with math how many bananas each monkey can have, in the fair ideal world.
+monkeys_fed = int(bananas / monkeys)
+
+print(f"My banana farm has {monkeys} monkeys, and {bananas} bananas.  That means we can feed each monkey {monkeys_fed} bananas.  These monkeys are going to eat well.")
+

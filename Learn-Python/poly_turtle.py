5c5ab8d0b530d10832319cf1612b8892c91f8ee7
Ryan Brooks <ryan.brooks@outlook.com>
2020-02-14 06:41:22 -0600

    Adding polygon turtle for loop

---
+++ Learn-Python/poly_turtle.py
@@ -0,0 +1,18 @@
+import turtle
+
+polygon = turtle.Turtle()
+num_sides = [3,4,5,6,7,8,9,10,11]
+side_length = 100
+
+def poly_turtle():
+        angle = 360.0 / s
+        polygon.forward(side_length)
+        polygon.right(angle)
+
+
+for i in range(500):
+    for s in num_sides:
+        s = int(s)
+        poly_turtle()
+
+turtle.done()

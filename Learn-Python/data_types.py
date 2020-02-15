7316e4613f024cfa98f1665d9e5adec01acac525
Ryan Brooks <ryan.brooks@outlook.com>
2020-02-15 05:28:28 -0600

    Adding length script.

---
+++ Learn-Python/data_types.py
@@ -0,0 +1,6 @@
+# integers = 5, 10, 20
+# float = 2.43
+# string = "Hold words"
+# list - ['banana', 'apple', 'oranges']
+# tuple - ('banana', 'apple', 'oranges')
+# dictionary = {"John": '123-900-1234', "Billy":'909-121-4444'}
---
+++ Learn-Python/exercise3_len.py
@@ -0,0 +1,11 @@
+print("What word would you like to measure?")
+word = input()
+count = int()
+
+for i in word:
+    print(i)
+    count += 1
+
+print(f"{word} has {count} characters.")
+
+    

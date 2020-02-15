9c2b36ac7b2679bb42ede315510b08fe00cc875f
Ryan Brooks <ryan.brooks@outlook.com>
2020-02-15 06:17:33 -0600

    Created len function in exercise3_len.py

--- Learn-Python/exercise3_len.py
+++ Learn-Python/exercise3_len.py
@@ -1,11 +1,16 @@
 print("What word would you like to measure?")
 word = input()
-count = int()
 
-for i in word:
-    print(i)
-    count += 1
+def count_words(word):
+    count = 0
+    for i in word:
+        print(i)
+        count = count + 1
+    return count
 
-print(f"{word} has {count} characters.")
+
+# Why doesn't this work?
+len = count_words(word)
+print(f"The word count for {word} is: {len}")
 
     
